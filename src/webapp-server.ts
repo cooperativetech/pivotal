c5e3eaf504db675bbfb4e283e3e07272e6bd6c11
Anand Shah <anand.v.shah@hotmail.com>
2025-08-28 19:31:48 -0400

    still kinda hacky. don't review yet.

--- src/webapp-server.ts
+++ src/webapp-server.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { serve } from '@hono/node-server'
 import { webappRoutes } from './webapp/routes'
-
+//
 const PORT = 3000
 const webapp = new Hono()
   .use(logger((message) => {
